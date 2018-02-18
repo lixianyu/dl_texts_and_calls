a4ccfb95a832c57b570d3b66c9ae2f124aece0d5
lixianyu <lbonus@163.com>
2018-02-18 13:27:20 +0800

    Finish Task1.

--- Task1.py
+++ Task1.py
@@ -6,14 +6,27 @@ import csv
 with open('texts.csv', 'r') as f:
     reader = csv.reader(f)
     texts = list(reader)
+    with open('calls.csv', 'r') as f:
+    	reader = csv.reader(f)
+    	calls = list(reader)
 
-with open('calls.csv', 'r') as f:
-    reader = csv.reader(f)
-    calls = list(reader)
+    	all_phone_number = set()
+
+    	for call_num in calls:
+    		all_phone_number.add(call_num[0])
+    		all_phone_number.add(call_num[1])
+    	for sms in texts:
+    		all_phone_number.add(sms[0])
+    		all_phone_number.add(sms[1])
+    	print("There are {} different telephone numbers in the records.".format(len(all_phone_number)))
+
+
+
 
 
 """
 任务1：
 短信和通话记录中一共有多少电话号码？每个号码只统计一次。
 输出信息：
-"There are <count> different telephone numbers in the records.""""
+"There are <count> different telephone numbers in the records."
+"""
