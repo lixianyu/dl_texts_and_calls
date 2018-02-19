72bed887f2055fd2b1fca2426619024646b1f276
lixianyu <lbonus@163.com>
2018-02-19 13:27:23 +0800

    Finished Task4.

--- Task4.py
+++ Task4.py
@@ -12,6 +12,29 @@ with open('calls.csv', 'r') as f:
     reader = csv.reader(f)
     calls = list(reader)
 
+#返回只拨打电话的set
+def getAllZhuJiao(calls):
+    zhujiao = set()
+    beijiao = set()
+    for call in calls:
+        zhujiao.add(call[0])
+        beijiao.add(call[1])
+    for ph in beijiao:
+        if ph in zhujiao:
+            zhujiao.remove(ph)
+    return zhujiao
+
+allZhu = getAllZhuJiao(calls)
+for sms in texts:
+    if sms[0] in allZhu:
+        allZhu.remove(sms[0])
+    if sms[1] in allZhu:
+        allZhu.remove(sms[1])
+
+allZhuList = sorted(list(allZhu))
+
+print("These numbers could be telemarketers: \n" + "\n".join(allZhuList))
+
 """
 任务4:
 电话公司希望辨认出可能正在用于进行电话推销的电话号码。
